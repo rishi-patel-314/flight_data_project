ec368972e048e55bea5c3922d5e4726fba4a1988
rishikanaujia <python.institute.llp@gmail.com>
2024-11-12 10:24:12 +0530

    refactor logging_config.py

--- constants/config.py
+++ constants/config.py
@@ -8,3 +8,7 @@ MAX_FLIGHT_DURATION = 5 * 60 * 60  # 5 hours in seconds
 MIN_PASSENGERS = 10
 MAX_PASSENGERS = 300
 NULL_PROBABILITY = 0.001  # Probability of introducing null values
+
+#log_configs
+MAX_LOG_SIZE= 25 * 1024 * 1024
+BACKUP_COUNT= 5
